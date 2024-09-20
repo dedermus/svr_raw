8941c182d6424890f2abc0973fc78c0b6f166e08
Aleksandr Domanskiy <spirit412@gmail.com>
2024-09-20 12:59:59 +0300

    commit 1.0.5

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use Svr\Raw\Controllers\FromSelexSheepController;
 use Svr\Raw\Controllers\FromSelexBeefController;
 use Svr\Raw\Controllers\FromSelexMilkController;
 
-Route::resource('svr-from_selex_milk', FromSelexSheepController::class);
-Route::resource('svr-from_selex_beef', FromSelexBeefController::class);
-Route::resource('svr-from_selex_sheep', FromSelexMilkController::class);
+Route::resource('from_selex_milk', FromSelexSheepController::class);
+Route::resource('from_selex_beef', FromSelexBeefController::class);
+Route::resource('from_selex_sheep', FromSelexMilkController::class);
