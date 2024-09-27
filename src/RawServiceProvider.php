d83908935fd56af0f40370053aa05254e0d7548f
Aleksandr Domanskiy <spirit412@gmail.com>
2024-09-27 11:51:46 +0300

    add lang resource commit 1.0.20

--- src/RawServiceProvider.php
+++ src/RawServiceProvider.php
@@ -9,12 +9,12 @@ class RawServiceProvider extends ServiceProvider
     /**
      * {@inheritdoc}
      */
-    public function boot(RawManager $extension)
+    public function boot()
     {
 
         if ($this->app->runningInConsole()) {
             $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-            $this->publishes([__DIR__.'/../resources/lang' => resource_path('lang')]);
+            $this->publishes([__DIR__.'/../resources/lang' => resource_path('lang')], 'open-admin-lang');
         }
 
         RawManager::boot();
