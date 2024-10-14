45b6af377e943ddc23da83735de3a70310f03e98
Aleksandr Domanskiy <spirit412@gmail.com>
2024-10-14 15:44:38 +0300

    migrate schema 1.0.28

---
+++ database/migrations/2024_09_06_000001_create_raw_schemas.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use Svr\Core\Enums\ApplicationStatusEnum;
+use Svr\Core\Enums\HerriotErrorTypesEnum;
+use Svr\Core\Enums\ImportStatusEnum;
+use Svr\Core\Enums\SystemBreedingValueEnum;
+use Svr\Core\Enums\SystemNotificationsTypesEnum;
+use Svr\Core\Enums\SystemParticipationsTypesEnum;
+use Svr\Core\Enums\SystemSexEnum;
+use Svr\Core\Enums\SystemStatusConfirmEnum;
+use Svr\Core\Enums\SystemStatusDeleteEnum;
+use Svr\Core\Enums\SystemStatusEnum;
+use Svr\Core\Enums\SystemStatusNotificationEnum;
+use Svr\Core\Enums\ApplicationAnimalStatusEnum;
+
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Проверить существование схемы raw (если нет, то создать ее)
+        DB::statement('CREATE SCHEMA IF NOT EXISTS raw');
+        DB::statement("COMMENT ON SCHEMA data IS 'Необработанные данные'");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::statement('DROP SCHEMA IF EXISTS raw CASCADE');
+
+    }
+};
