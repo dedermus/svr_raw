53a6ad1adc8f3b5b9aa0fbc170a2bcbc4341bfa2
Aleksandr Dom <spirit412@gmail.com>
2024-10-17 16:05:04 +0300

    edit phpunit.xml 1.0.38

--- database/migrations/2024_09_06_000001_create_raw_schemas.php
+++ database/migrations/2024_09_06_000001_create_raw_schemas.php
@@ -2,18 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
-use Svr\Core\Enums\ApplicationStatusEnum;
-use Svr\Core\Enums\HerriotErrorTypesEnum;
-use Svr\Core\Enums\ImportStatusEnum;
-use Svr\Core\Enums\SystemBreedingValueEnum;
-use Svr\Core\Enums\SystemNotificationsTypesEnum;
-use Svr\Core\Enums\SystemParticipationsTypesEnum;
-use Svr\Core\Enums\SystemSexEnum;
-use Svr\Core\Enums\SystemStatusConfirmEnum;
-use Svr\Core\Enums\SystemStatusDeleteEnum;
-use Svr\Core\Enums\SystemStatusEnum;
-use Svr\Core\Enums\SystemStatusNotificationEnum;
-use Svr\Core\Enums\ApplicationAnimalStatusEnum;
 
 
 return new class extends Migration {
