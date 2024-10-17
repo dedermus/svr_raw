e61b0b9faa90fa85db98ae1c796343fbbfdd3353
Aleksandr Dom <spirit412@gmail.com>
2024-10-17 17:54:48 +0300

    попытка создать тесты пакета

---
+++ tests/Unit/ModelFromSelexMilkTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Svr\Raw\Tests\Unit;
+
+use Illuminate\Foundation\Testing\TestCase;
+use OpenAdminCore\Admin\Auth\Database\Administrator;
+
+
+class ModelFromSelexMilkTest extends TestCase
+{
+
+
+    public function testModelFromSelexMilk()
+    {
+        echo "############### testModelFromSelexMilk";
+    }
+
+}
