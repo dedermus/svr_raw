b23afce5adbf1f29df1e81e7aea99f9ba18ef39f
Aleksandr Dom <spirit412@gmail.com>
2024-11-01 11:13:59 +0300

    Рефакторинг Api роутеров `api/from-selex-beef`

--- routes/Api/api.php
+++ routes/Api/api.php
@@ -1,5 +1,6 @@
 <?php
 
+//use Svr\Raw\Middleware\ApiValidationErrors;
 use Illuminate\Support\Facades\Route;
 use Svr\Raw\Controllers\Api\ApiFromSelexBeefController;
 
@@ -12,8 +13,9 @@ use Svr\Raw\Controllers\Api\ApiFromSelexBeefController;
 
 Route::middleware('api')
     ->prefix('api')
+//    ->middleware(ApiValidationErrors::class)
     ->group(function () {
-        Route::get('from-selex-beef/', [ApiFromSelexBeefController::class, 'index']);       // Для получения списка записей
-        Route::post('from-selex-beef/', [ApiFromSelexBeefController::class, 'store']);      // Для создания новой записи
-        Route::put('from-selex-beef/{id}', [ApiFromSelexBeefController::class, 'update']);  // Для обновления существующей записи
+        Route::get('from-selex-beef/',      [ApiFromSelexBeefController::class, 'index']);      // Для получения списка записей
+        Route::post('from-selex-beef/',     [ApiFromSelexBeefController::class, 'store']);      // Для создания новой записи
+        Route::put('from-selex-beef/{id}',  [ApiFromSelexBeefController::class, 'update']);     // Для обновления существующей записи
     });
